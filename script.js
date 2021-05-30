d596369f33f1f40ccc2926800bcb29d2b663eb98
Rohan Batra <rohanbatra24@gmail.com>
2021-05-29 22:11:56 -0400

    form validation using constraint API

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const form = document.getElementById("form");
+const password1El = document.getElementById("password1");
+const password2El = document.getElementById("password2");
+const messageContainer = document.querySelector(".messgae-container");
+const message = document.getElementById("message");
 
+let isValid = false;
+
+const validateForm = (params) => {
+  // using constraint API
+  isValid = form.checkValidity();
+  console.log(isValid);
+};
+
+const processFormData = (e) => {
+  e.preventDefault();
+  console.log(`e`, e);
+
+  // validate form
+  validateForm();
+};
+
+// event listener
+form.addEventListener("submit", processFormData);
